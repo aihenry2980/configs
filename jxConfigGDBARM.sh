1af243fec92fd39dba545b08a7901c359c69c1f6
JinXin <aihenry2980@gmail.com>
2020-03-14 05:38:47 -0700

    Add files via upload

---
+++ jxConfigGDBARM.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+./deleteCache.sh
+
+./configure --target=arm-linux-gnueabi --host=x86_64-linux-gnu --build=x86_64-linux-gnu CFLAGS_FOR_BUILD="-g -O0 -pg" CXXFLAGS_FOR_BUILD="-g -O0 -pg" CXXFLAGS="-g -O0 -pg" CFLAGS="-g -O0 -pg" CFLAGS_FOR_TARGET="-g -O0 -pg" CXXFLAGS_FOR_TARGET="-g -O0 -pg" 
